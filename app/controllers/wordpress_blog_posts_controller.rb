22acdcf0c417acaa1a21987df24e71017b716b45
Prelang Builder <builder@prelang.com>
2014-05-03 00:42:00 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/wordpress_blog_posts_controller.rb
+++ app/controllers/wordpress_blog_posts_controller.rb
@@ -1,2 +1,6 @@
 class WordpressBlogPostsController < InheritedResources::Base
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
 end
